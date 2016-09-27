cacd3ef06fc2c918fd92c910ce9da3a41cc82398
mrnafstad <mr.nafstad@gmail.com>
2016-09-27 17:51:23 +0200

    Added functions to find max_offdiag and to do rotations.

--- Code/bproject2.cpp
+++ Code/bproject2.cpp
@@ -31,5 +31,68 @@ void jacobi_method ( double ** A, double ** R int n )
 	cout <<"Number of iterations: " << iterations << "\n";
 	return;
 }
+//Function to find maximum matrix element.
 
+double maxoffdiag ( double **A, int * k, int * l, int n)
+{
+	double max = 0.0;
+
+	for ( int i = 0; i < n; i++ ) {
+		for ( int j = 0; j < n; j++ ) {
+			if ( fabs(A[i][j]) > max ) {
+				max  = fabs(A[i][j]);
+				*l = i;
+				*k = j;
+			}
+		}
+	}
+	return max;
+}
+
+//Function to find the values of cos and sin
+void rotate ( double ** A, double ** R, int k, int l, int n)
+{
+	double s, c;
+	if ( A[k][l] != 0.0) {
+		double t, tau;
+		tau = ( A[l][l] - A[k][k] ) / ( 2*A[k][l] );
+		if ( tau > 0 ) {
+			t = 1.0 / ( tau + sqrt( 1.0 + tau*tau ) );
+		}
+		else {
+			t = -1.0 / ( -tau + sqrt( 1.0 + tau*tau ) );
+		}
+
+		c = 1 / sqrt( 1 + t*t );
+		s = c * t;
+	}
+	else {
+		c = 1.0;
+		s = 0.0;
+	}
+	double a_kk, a_ll, a_ik, a_il, r_ik, r_il;
+	a_kk = A[k][k];
+	a_ll = A[l][l];
+	//changing the matrix elements with indices k and l
+	A[k][k] = c*c*a_kk - 2.0*c*s*A[k][l] + s*s*a_ll;
+	A[l][l] =  s*s*a_kk + 2.0*c*s*A[k][l] + c*c*a_ll;
+	A[k][l] = 0.0; //hardcoding of zeros
+	A[l][k] = 0.0;
+	//change remaining elements
+	for ( int i = 0; i < n; i++ ) {
+		if ( i != k && i != l ) {
+			a_ik = A[i][k];
+			a_il = A[i][l];
+			A[i][k] = c*a_ik - s*a_il;
+			A[k][i] = A[i][k];
+			A[i][l] = c*a_il + s*a_ik;
+			A[l][i] = A[i][l];
+		}
+		r_ik = R[i][k];
+		r_il = R[i][l];
+		R[i][k] = c*r_ik - s*r_il;
+		R[i][l] = c*r_il + s*r_ik;
+	}
+	return;
+}
 
