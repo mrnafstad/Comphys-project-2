daa6219285d15920f7c5b1d7cdf118619cd22e85
mrnafstad <mr.nafstad@gmail.com>
2016-09-27 18:02:04 +0200

    Fixed some syntax bugs. Need a main()

--- Code/bproject2.cpp
+++ Code/bproject2.cpp
@@ -1,36 +1,7 @@
 #include <cmath>
 #include <iostream>
+//#include "jacobi.h"
 
-using namespace.std;
-
-void jacobi_method ( double ** A, double ** R int n )
-{
-//Settup of eigenvector
-	for ( int i = 0; i < n; i++){
-		for ( int j = 0; j < n; j++){
-			if ( i == j){
-				R[i][j] = 1.0;
-			}
-			else{
-				R[i][j] = 0.0
-			}
-		}
-	}
-	int k, l;
-
-	double epsilon = 1.0e-8;
-	double max_number_iterations = (double) n * (double) n * (double) n;
-	int iterations = 0;
-	double max_offdiag = maxoffdiag ( A, &k, &l, n );
-
-	while ( fabs(max_offdiag) > epsilon && (double) iterations < max_number_iterations ){
-		max:offdiag = maxoffdiag( A, &k, &l, n );
-		rotate (A, R, k, l, n);
-		iterations++;
-	}
-	cout <<"Number of iterations: " << iterations << "\n";
-	return;
-}
 //Function to find maximum matrix element.
 
 double maxoffdiag ( double **A, int * k, int * l, int n)
@@ -95,4 +66,34 @@ void rotate ( double ** A, double ** R, int k, int l, int n)
 	}
 	return;
 }
+
+void jacobi_method ( double ** A, double ** R, int n )
+{
+//Settup of eigenvector
+	for ( int i = 0; i < n; i++){
+		for ( int j = 0; j < n; j++){
+			if ( i == j){
+				R[i][j] = 1.0;
+			}
+			else{
+				R[i][j] = 0.0;
+			}
+		}
+	}
+	int k, l;
+
+	double epsilon = 1.0e-8;
+	double max_number_iterations = (double) n * (double) n * (double) n;
+	int iterations = 0;
+	double max_offdiag = maxoffdiag ( A, &k, &l, n );
+
+	while ( fabs(max_offdiag) > epsilon && (double) iterations < max_number_iterations ){
+		max_offdiag = maxoffdiag( A, &k, &l, n );
+		rotate (A, R, k, l, n);
+		iterations++;
+	}
+	std::cout <<"Number of iterations: " << iterations << "\n";
+	return;
+}
+
 
